6f83a9972151d0d494a6514545d929fde7dcce2f
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-30 10:32:59 +0600

    average of four number in javaScript
    average of four number in javaScript(need to spicify the Number)

---
+++ average_4_number.js
@@ -0,0 +1,11 @@
+let num1 = prompt("Enter 1st Number: ");
+let num2 = prompt("Enter 2nd Number: ");
+let num3 = prompt("Enter 3rd Number: ");
+let num4 = prompt("Enter 4th Number: ");
+num1 = Number(num1);
+num2 = Number(num2);
+num3 = Number(num3);
+num4 = Number(num4);
+let ave = (num1 + num2 + num3 + num4) / 4;
+
+document.write("The average number is: " + ave);
